ddd1b5e92d43e35cc3c5f3a179563ca8f42b52f7
FePaschoal <112896945+FPPaschoal@users.noreply.github.com>
2022-09-23 22:00:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function mostrar(ID){
+
+   
+    const opcoes = document.getElementById(ID)
+
+        opcoes.style.display = "block"
+}
+
+function sumir(ID){
+    const opcoes = document.getElementById(ID)
+
+    opcoes.style.display = "none"
+}
+
